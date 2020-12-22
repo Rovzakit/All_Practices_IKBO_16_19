e64b5f88edd0123a2d0836d05044670f889e9bb4
Rovzakit <70742212+Rovzakit@users.noreply.github.com>
2020-12-22 15:52:34 +0300

    Add files via upload

---
+++ src/Practice25_26/Main25_26.java
@@ -0,0 +1,9 @@
+package Practice25_26;
+
+import java.io.IOException;
+
+public class Main25_26 {
+    public static void main(String[] args) throws IOException {
+        GetMetro getmetro = new GetMetro();
+    }
+}
